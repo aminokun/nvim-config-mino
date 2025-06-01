b86e912e8575a2c59a0ec7ac8899c9607669ca44
aminokun <amine.dbh99@gmail.com>
2025-06-01 19:27:40 +0200

    added telescope and keybindings

--- init.lua
+++ init.lua
@@ -1,7 +1,4 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
 require("config.lazy")
 require("lazy").setup("plugins")
+require "user.keymaps"
+require "user.options"
